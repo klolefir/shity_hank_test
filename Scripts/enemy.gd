8d1d7dc43f8cf5b38aa415aae76f2eec31ed961d
klolefir <denboy145@mail.ru>
2023-03-08 19:19:54 +0300

    add ai

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -11,7 +11,8 @@ var current_state : States
 var navigation_agent : NavigationAgent3D
 @export var way_points : Array
 var waypoint_index : int
-@export var speed = 2
+@export var chase_speed = 2
+@export var patrol_speed = 2
 
 var patrol_timer : Timer
 
@@ -19,12 +20,15 @@ var player_ears_far : bool
 var player_ears_close : bool
 var player_slight_close : bool
 
+var player
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	current_state = States.patrol
 	waypoint_index = 0
 	navigation_agent = $NavigationAgent3D
 	patrol_timer = $PatrolTimer
+	player = get_tree().get_nodes_in_group("Player")[0]
 
 	way_points = get_tree().get_nodes_in_group("EnemyWaypoint")
 	navigation_agent.target_position = way_points[0].global_position
@@ -39,22 +43,63 @@ func _process(delta):
 				current_state = States.waiting
 				patrol_timer.start()
 				return
-			var target_pos = navigation_agent.get_next_path_position()
-			var direction = global_position.direction_to(target_pos)
-			FaceDirection(target_pos)
-			velocity = direction * speed	
-			move_and_slide()
-			if player_ears_far:
-				CheckPlayer()
-
+			MoveTowardsPoint(delta, patrol_speed)
+			#print("patrol player")
 		States.chasing:
+			if navigation_agent.is_navigation_finished():
+				patrol_timer.start()
+				current_state = States.waiting
+			navigation_agent.target_position = player.global_position
+			MoveTowardsPoint(delta, chase_speed)
+			print("chasing player")
 			pass
 		States.hunting:
+			if navigation_agent.is_navigation_finished():
+				patrol_timer.start()
+				current_state = States.waiting
+			MoveTowardsPoint(delta, chase_speed)
+			print("hunting player")
 			pass
-		States.waiting: pass
+		States.waiting:
+			#print("wainting")
+			CheckPlayer()
+			
+
+func MoveTowardsPoint(delta, speed):
+	var target_pos = navigation_agent.get_next_path_position()
+	var direction = global_position.direction_to(target_pos)
+	FaceDirection(target_pos)
+	velocity = direction * speed	
+	move_and_slide()
+	if player_ears_far:
+		CheckPlayer()
+
+
 
 func CheckPlayer():
-	pass 
+	var space_state = get_world_3d().direct_space_state
+	var result = space_state.intersect_ray(PhysicsRayQueryParameters3D.create($Head.global_position, player.get_node("camera").global_position, 1, [self.get_rid()])) 
+	if result.size() > 0:
+		if result["collider"].is_in_group("Player"):
+			if player_ears_close:
+				#if result["collider"].crouched == false:
+				current_state = States.chasing
+				print("Hear player close")
+
+			if player_ears_far:
+				#if result["collider"].crouched == false:
+				current_state = States.hunting
+				navigation_agent.target_position = player.global_position
+				print("Hear player far")
+
+			if player_slight_close:
+				current_state = States.chasing
+				navigation_agent.target_position = player.global_position
+				print("Saw player")
+
+			#if player_slight_far:
+			#	if result["collider"].crouched = false:
+			#		current_state = States.hunting
 
 func FaceDirection(direction : Vector3):
 	look_at(Vector3(direction.x, global_position.y, direction.z), Vector3.UP)
@@ -65,34 +110,33 @@ func _on_patrol_timer_timeout():
 	if waypoint_index > way_points.size() - 1:
 		waypoint_index = 0
 	navigation_agent.target_position = way_points[waypoint_index].global_position
-	pass # Replace with function body.
 
 func _on_hearing_far_body_entered(body):
 	if body.is_in_group("Player"):
+		print("hear far enter")
 		player_ears_far = true
-		pass
 
 func _on_hearing_far_body_exited(body):
 	if body.is_in_group("Player"):
+		print("hear far exit")
 		player_ears_far = false
-		pass
 
 func _on_hearing_close_body_entered(body):
 	if body.is_in_group("Player"):
+		print("hear close enter")
 		player_ears_close = true
-		print("test")
-	pass
 
 func _on_hearing_close_body_exited(body):
 	if body.is_in_group("Player"):
+		print("hear close exit")
 		player_ears_close = false
-		pass
 
 func _on_slight_close_body_entered(body):
 	if body.is_in_group("Player"):
-		player_slight_close = false
-		pass
+		print("slight close enter")
+		player_slight_close = true
 
 func _on_slight_close_body_exited(body):
 	if body.is_in_group("Player"):
+		print("slight close exit")
 		player_slight_close = false
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,14 +1,17 @@
 extends CharacterBody3D
 
 const SPEED = 5.0
+const CROUCH_SPEED = 2.0
 const JUMP_VELOCITY = 4.5
 @export var sensitivity = 3
 var FlashLightIsOut : bool
+var crouched : bool
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _ready():
+	crouched = false
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta):
@@ -32,12 +35,27 @@ func _physics_process(delta):
 			$AnimationPlayer.play("FlashLightShow")
 		FlashLightIsOut = !FlashLightIsOut
 
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	var local_speed : float
+	if Input.is_action_pressed("Crouch"):
+		local_speed = CROUCH_SPEED	
+		if !crouched:
+			$AnimationPlayer.play("Crouch")
+			crouched = true
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		local_speed = SPEED
+		if crouched:
+			var space_state = get_world_3d().direct_space_state
+			var result = space_state.intersect_ray(PhysicsRayQueryParameters3D.create(position, position + Vector3(0, 2, 0), 1, [self]))
+			if result.size() == 0:
+				$AnimationPlayer.play("UnCrouch")
+				crouched = false
+
+	if direction:
+		velocity.x = direction.x * local_speed
+		velocity.z = direction.z * local_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, local_speed)
+		velocity.z = move_toward(velocity.z, 0, local_speed)
 
 	move_and_slide()
 
@@ -45,5 +63,5 @@ func _input(event):
 	if event is InputEventMouseMotion:
 		rotation.y -= event.relative.x / 1000 * sensitivity
 		$camera.rotation.x -= event.relative.y / 1000 * sensitivity
-		rotation.x = clamp(rotation.x, PI/2, -PI/2)
-		rotation.x = clamp($camera.rotation.x, -2, 2)
+		rotation.x = clamp(rotation.x, -PI/2, PI/2)
+		$camera.rotation.x = clamp($camera.rotation.x, -2, 2)
